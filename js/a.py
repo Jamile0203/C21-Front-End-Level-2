1d9ad36645b9187118a11cc0763c129bbdb8ddca
Jamile Yaacob <jamile.yaacob@student.rocva.nl>
2021-11-14 18:34:21 +0100

    opdracht 7

---
+++ js/a.py
@@ -0,0 +1,13 @@
+def my_function(fmovie):
+    print("hallo")
+    input()
+    print("Wat is jouw favoriete film?")
+    inp = input()
+    if(inp == fmovie):
+      print("Leuke film!")
+    else:
+        print("Ik vind dat film niet leuk")
+
+my_function("My neighbor Totoro")
+
+fmovie = "My neighbor Totoro"
